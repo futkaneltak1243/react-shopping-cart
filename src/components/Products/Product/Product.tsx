a26c87f9e037559b0294faa185659a86b28bac68
futkaneltak1243 <ft.142001@gmail.com>
2025-09-11 13:42:27 +0300

    adding Product component + img optimzation

--- src/components/Products/Product/Product.tsx
+++ src/components/Products/Product/Product.tsx
@@ -1,11 +1,38 @@
-const Product = () => {
+import { Heart } from "lucide-react"
+
+type ProductProps = {
+    title: string
+    description: string
+    price: number
+    image: string
+
+}
+
+const Product = ({ title, description, price, image }: ProductProps) => {
     return (
-        <div>
-            <div></div>
-            <div>
-                <img src="" />
+        <div className="w-[223px] h-[450px] bg-white shadow-[2px_1px_10px_0px_rgba(0,0,0,0.25)] relative">
+            <div className="w-[31px] h-[31px] rounded-full bg-white
+            flex justify-center items-center absolute top-[11px] right-[12px]
+            cursor-pointer shadow-[0px_4px_4px_4px_rgba(0,0,0,0.25)]">
+                <Heart className="h-[20px] w-[20px]" />
             </div>
-            <div></div>
+            <div className="w-full h-[292px]">
+                <img src={image} className="object-cover w-full h-full" />
+            </div>
+            <div className="p-2 h-[80px]">
+                <h2 className="font-semibold">{title}</h2>
+                <p className="text-gray-500 text-sm mt-2">{description}</p>
+
+            </div>
+            <p className="font-semibold mt-1 pl-2">
+                {price}
+                <span className="ml-1 text-xs font-medium">$</span>
+            </p>
+            <button className="bg-[#A0AD7B] hover:bg-[#8f9c6b] w-[188px] h-[30px] text-white 
+                absolute cursor-pointer bottom-3 left-1/2 -translate-x-1/2">
+                Add to cart
+            </button>
+
         </div>
     )
 }
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,6 +1,7 @@
 import SearchBar from "./SearchBar"
 import Filters from "./Filters"
 import Product from "./Product"
+import data from "../data/products.json"
 
 const SearchFilters = () => {
     return (
@@ -13,9 +14,14 @@ const SearchFilters = () => {
 
 const Products = () => {
     return (
-        <div className="mt-[58px] ">
+        <div className="mt-[58px] mb-10">
             <SearchFilters />
-            <Product />
+            <div className="flex justify-center items-center mt-[74px]">
+                <div className="grid grid-cols-5 gap-4">
+                    {data.map(product => <Product title={product.name} description={product.description} price={product.price} image={product.image}></Product>)}
+                </div>
+            </div>
+
         </div>
     )
 }
